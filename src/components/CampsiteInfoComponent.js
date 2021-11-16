fd15209b225f9a44927dfc9750f69aad5ff35bad
Joe Conner <warriorwebworx@outlook.com>
2021-11-16 14:22:00 -0800

    Components update

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+
+
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        const campsite = this.props.
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+    
+export default CampsiteInfo;
